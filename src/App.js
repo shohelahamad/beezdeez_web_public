b11847d0321492612d7c40e9cb1c33c88198f9a9
developer <shohelahamad@gmail.com>
2019-09-06 13:15:26 +0200

    init beezdeez

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo-stl" alt="logo" />
+        <h3>
+          We are coming soon to make your daily routine smarter....
+        </h3>
       </header>
     </div>
   );
