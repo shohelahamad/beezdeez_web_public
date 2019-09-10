092fc7fad2e999fa2a7c1f9a9e0ad2bd7dc8126b
developer <shohelahamad@gmail.com>
2019-09-10 16:49:08 +0200

    updats

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
-
+import Navbar from './components/navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo-stl" alt="logo" />
         <h3>
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div >
+        <h3>
+          This section is a navbar
+        </h3>
+    </div>
+  );
+}
+
+export default Navbar;
