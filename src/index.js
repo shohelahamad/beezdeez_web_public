a4f2e2109a22b60b08facf7d776500848f70446f
Md Shohel Ahamad <shohel.ahamad@alphajump.de>
2020-09-02 12:12:04 +0200

    new changes

--- src/index.js
+++ src/index.js
@@ -2,29 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import axios from 'axios';
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
-axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+import * as serviceWorker from './serviceWorker';
 
-axios.interceptors.request.use(request => {
-    console.log(request);
-    // Edit request config
-    return request;
-}, error => {
-    console.log(error);
-    return Promise.reject(error);
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-axios.interceptors.response.use(response => {
-    console.log(response);
-    // Edit request config
-    return response;
-}, error => {
-    console.log(error);
-    return Promise.reject(error);
-});
-
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- src/store/actions/index.js
+++
@@ -1,3 +0,0 @@
-export { tryAuth, logout } from "./auth";
-export { editprofile, setprofile, getprofile } from "./profile";
-export { addSchedule, getSchedules, updateSchedule, deleteSchedule } from "./schedule";
